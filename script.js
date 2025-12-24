bf074908100706d24d083749843a0c9b86369c47
Bell <210176980+robothello444@users.noreply.github.com>
2025-12-24 12:43:46 -0600

    Add resize button

--- script.js
+++ script.js
@@ -11,13 +11,29 @@ function generateGrid(x,y) {
         const square = document.createElement("div");
         square.classList.add("square");
         square.addEventListener("mouseover", (event) => {
-            // console.log(`Hover! ${[i]}`);
+            // console.log(`Hover! ${[i + 1]}`); // Debug
             square.classList.add("hover-square");
         });
     container.appendChild(square);
     };
     
-    container.setAttribute("style", `width: ${(x * 10)}px; height: ${(x * 10)}px;`);
+    container.setAttribute("style", `width: ${(x * 16)}px; height: ${(x * 16)}px;`);
 };
 
 const firstRun = generateGrid(16, 16);
+
+const btnResize = document.querySelector(".resize-grid");
+btnResize.addEventListener("click", function resizeGrid() {
+    var newResize = prompt("Write the width and height of the grid in squares, i.e. '16x16' (Limit = 100)").split('x');
+    // console.log(newResize);
+    let newResizeX = newResize[0];
+    let newResizeY = newResize[1];
+
+    // Checks user inputs
+    if (newResizeX <= 100 && newResizeY <= 100) {
+        generateGrid(newResizeX, newResizeY);
+    } else {
+        alert("Try again.");
+        resizeGrid();
+    };
+});
