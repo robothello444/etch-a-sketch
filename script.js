2aea50d76034a16a77e9a9dd5e63b492e8d68bb7
Bell <210176980+robothello444@users.noreply.github.com>
2025-12-24 19:07:05 -0600

    Add calculate square size and changes to Resize button

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
 
 const container = document.querySelector(".container");
+const sideNumberContainer = document.querySelector(".number-count");
 
-function generateGrid(x,y) {
+function generateGrid(x) {
     // Remove previous grid before generates one new
     while (container.firstChild) {
     container.removeChild(container.lastChild);
     };
+
+    const squareSize = (400 / x) - 2; // (-2) because of the grid lines
     
-    for (let i = 0; i < (x * y); i++) {
+    for (let i = 0; i < (x * x); i++) {
         const square = document.createElement("div");
+
         square.classList.add("square");
+        square.setAttribute("style", `width: ${squareSize}px; height: ${squareSize}px`);
         square.addEventListener("mouseover", (event) => {
             // console.log(`Hover! ${[i + 1]}`); // Debug
             square.classList.add("hover-square");
         });
     container.appendChild(square);
     };
-    
-    container.setAttribute("style", `width: ${(x * 16)}px; height: ${(x * 16)}px;`);
+
+    sideNumberContainer.textContent = `${x}x${x}`
 };
 
-const firstRun = generateGrid(16, 16);
+const firstRun = generateGrid(16);
 
 const btnResize = document.querySelector(".resize-grid");
 btnResize.addEventListener("click", function resizeGrid() {
-    var newResize = prompt("Write the width and height of the grid in squares, i.e. '16x16' (Limit = 100)").split('x');
+    var newResize = prompt("Write how many squares of length will have on each side (Limit = 100)");
     // console.log(newResize);
-    let newResizeX = newResize[0];
-    let newResizeY = newResize[1];
-
     // Checks user inputs
-    if (newResizeX <= 100 && newResizeY <= 100) {
-        generateGrid(newResizeX, newResizeY);
+    if (newResize <= 100) {
+        generateGrid(newResize);
     } else {
         alert("Try again.");
         resizeGrid();
